a443555d1302082988616a42fd723eb0773b63c7
Arun K Bharathan <neutroproto@gmail.com>
2015-06-06 00:35:42 +0530

    jun_6

---
+++ Exercise_1_20.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#define MAXLINE 1000
+#define TABSTOP 4
+
+int getlyne(char line_input[], int lim);
+int detab(char input[],char output[],int k);
+main()
+{
+
+    char input[MAXLINE],output[MAXLINE];
+    int index[100];
+    int len = 0,c=0,i=0,j=0,k=0;
+    for(i=0;i<100;i++)
+    index[i]=-1;
+    while((len = getlyne(input,MAXLINE)) > 0)
+    {
+        if(len>MAXLINE)
+        {
+            printf("\nLine length %d larger than MAXLINE %d.\nIncrease array limit. \n\nProgram output not correct",len,MAXLINE);
+            break;
+        }
+        if(len == 0)
+        break;
+
+ k=detab(input,output,k);
+
+
+
+
+
+
+
+
+}
+output[k]='\0';
+printf("\n%s",output);
+return 0;
+    }
+
+
+int getlyne(char s[],int lim)
+{
+int c, i;
+
+for (i=0; (c=getchar())!=EOF && c!='\n'; ++i)
+s[i] = c;
+if (c == '\n') {
+s[i] = c;
+++i;
+}
+s[i] = '\0';
+return i;
+}
+
+int detab(char input[],char output[],int k)
+{
+    int j=0,i=0;
+    for(i=0;input[i]!='\n';i++)
+{
+    if(input[i] == '\t')
+    {
+        for(j=0;j<TABSTOP;j++)
+        {
+        output[k] = ' ';
+        k++;
+        }
+
+    }
+    else
+    {
+        output[k] = input[i];
+        k++;
+    }
+
+}
+output[k] = '\n';
+        k++;
+return k;
+}
+
