471b4720e361b61f3797ae0d42c13545b601df3d
Arun K Bharathan <neutroproto@gmail.com>
2015-06-05 21:39:46 +0530

    last

--- Exercise_1_18.c
+++ Exercise_1_18.c
@@ -7,9 +7,9 @@ int getlyne(char line_input[], int lim);
 main()
 {
 
-    char input[MAXLINE];
+    char input[MAXLINE],output[MAXLINE];
     int index[100];
-    int len = 0,c=0,i=0,j=0;
+    int len = 0,c=0,i=0,j=0,k=0;
     for(i=0;i<100;i++)
     index[i]=-1;
     while((len = getlyne(input,MAXLINE)) > 0)
@@ -19,61 +19,52 @@ main()
             printf("\nLine length %d larger than MAXLINE %d.\nIncrease array limit. \n\nProgram output not correct",len,MAXLINE);
             break;
         }
+        if(len == 0)
+        break;
+c=len-1;i=0;j=0;
 
-
-        for(i=len;i>=0;i--)
-        {
-           if(input[i]=='\n')
-              {index[c]=i;c++;}
-        }
-        index[c]=0;
-             for(i=0;i<=c+1;i++)
-             printf("%d==>%d\n",i,index[i]);
- putchar('\n');
-        for(i=0;i<c;i++)
-      {  for(j=index[i]-1;j>index[i+1]+1;j--)
-        {  /*putchar(input[j]);*/
-            if(input[j] > ' ')
-                {
-
-                continue;
-                }
-            else if((input[j] == ' ') || (input[j] == '\t') )
-            {
-                input[j]='\0';
-             /*   putchar(j);*/
-
-            }
-
-        }
-        putchar('\n');
-        }
-
-    /*    putchar('\n');
-        for(i=0;i<len;i++)
-            putchar(input[i]);*/
-
-for(i=0;i<len;i++)
-{putchar(input[i]);}
-
-
-
+while(input[c] < '0')
+{
+i++;
+input[c]='\0';
+j=0;
+if(c==0)
+{j=1;
 break;
 }
+c--;
+}
+if(j==0)
+{
+  for(i=0;input[i]!='\0';i++)
+    {output[k]=input[i];
+        k++;
+    }
+    output[k]='\n';k++;
+}
 
+
+
+
+
+}
+output[k]='\0';
+printf("\n%s",output);
 return 0;
     }
 
 
-int getlyne(char line_input[], int lim)
+int getlyne(char s[],int lim)
 {
-    int i;
-    char c;
-    for(i=0 ; (c=getchar()) != EOF ; ++i )
-        line_input[i] = c;
+int c, i;
 
-    line_input[i] = '\0';
-
-    return i;
+for (i=0; (c=getchar())!=EOF && c!='\n'; ++i)
+s[i] = c;
+if (c == '\n') {
+s[i] = c;
+++i;
+}
+s[i] = '\0';
+return i;
 }
 
