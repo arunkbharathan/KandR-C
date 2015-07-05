0388fe6ffe647c7b9f47b102d41246d62610ab0a
arunkbharathan <arunkbharathan@gmail.com>
2015-07-06 00:39:16 +0530

    Chapter 2 first exercise

---
+++ Chapter_2/infi.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+long double two_raiseto_ld(unsigned int);
+
+
+void main()
+{
+unsigned int i;
+char bits[]= {0xff, 0xff, 0xff, 0xff, 0xff ,0xff ,0xff ,0xff ,0xfe ,0x7f};
+long double ld_max =*(long double*)bits;
+//infinity = two_raiseto_ld(16384);
+//if(infinity == two_raiseto_ld(16383))
+//printf("yes");
+//for(i=0;i<16;i++)
+//printf("%x ",bits[i]);
+
+printf("\n%Lf\n",ld_max);
+}
+
+
+long double  two_raiseto_ld(unsigned int  exponent)
+{
+  unsigned int i=1;
+  long double temp=1;
+  while(i<=exponent)
+  {
+    temp*=2;
+    i++;
+  }
+  
+  
+  return temp;
+}
---
+++ Chapter_2/ranges_from_header.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+
+  
+   
+      printf("unsigned char range 0 to %d\n",UCHAR_MAX);
+      printf("unsigned short int range 0 to %u\n",USHRT_MAX);
+      printf("unsigned int range 0 to %u\n",UINT_MAX);
+      printf("unsigned long int range 0 to %lu\n",ULONG_MAX);
+      printf("unsigned long long int range 0 to %llu\n\n",ULLONG_MAX);
+      
+      printf("signed char range %d to %d\n",SCHAR_MIN,SCHAR_MAX);
+      printf("signed short int range %d to %d\n",SHRT_MIN,SHRT_MAX);
+      printf("signed int range %d to %d\n",INT_MIN,INT_MAX);
+      printf("signed long int range %ld to %ld\n",LONG_MIN,LONG_MAX);
+      printf("signed long long int range %lld to %lld\n",LLONG_MIN,LLONG_MAX);
+
+   
+  
+   return(0);
+}
